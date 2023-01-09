93cb227ff65b49ad998fdddf4422e67d3e4bcfb6
KrzysztofBurminski <k.burminski@clearcode.cc>
2023-01-09 15:56:45 +0100

    Added Cypress with simple test

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+describe('template spec', () => {
+  it('Should login', () => {
+    cy.visit('/');
+    cy.get(".sc-pVTFL").click();
+    cy.get("input[name='email']").type("test@test.com");
+    cy.get("input[name='password']").type("test123");
+    cy.get("button[type='submit']").click();
+    cy.url().should('eq', Cypress.config().baseUrl +  "/WatchTime_React/profile");
+  })
+})
