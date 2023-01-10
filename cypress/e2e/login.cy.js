137f8a759a4b3c4fe40e86217ec6f5bd5433d58e
KrzysztofBurminski <k.burminski@clearcode.cc>
2023-01-10 12:54:05 +0100

    Added fail test, checking ss CI feature

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,7 +4,7 @@ describe('template spec', () => {
     cy.visit('/');
     cy.get(".sc-pVTFL").click();
     cy.get("input[name='email']").type("test@test.com");
-    cy.get("input[name='password']").type("test123");
+    cy.get("input[name='password']").type("test1234");
     cy.get("button[type='submit']").click();
     cy.url().should('eq', Cypress.config().baseUrl +  "/WatchTime_React/profile");
   })
