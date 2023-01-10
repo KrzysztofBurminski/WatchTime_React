28822b528d80393e0138847ef46a8789abcbf6ce
KrzysztofBurminski <k.burminski@clearcode.cc>
2023-01-10 12:35:24 +0100

    Added fail test, checking ss CI feature

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots'
   },
 });
