830e6c042eebff8632b40fd09d2d7a777e79b71a
KrzysztofBurminski <k.burminski@clearcode.cc>
2023-01-10 13:52:01 +0100

    changed resolutions of screen size in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-
+    viewportHeight: 2160,
+    viewportWidth: 3840,
   },
 });
