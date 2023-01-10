5a8c1dd76f74d0bf629886c82b57fc4efbe0c180
KrzysztofBurminski <k.burminski@clearcode.cc>
2023-01-10 12:38:31 +0100

    Revert "Added fail test, checking ss CI feature"
    This reverts commit 28822b528d80393e0138847ef46a8789abcbf6ce.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    screenshotOnRunFailure: true,
-    screenshotsFolder: 'cypress/screenshots'
+
   },
 });
